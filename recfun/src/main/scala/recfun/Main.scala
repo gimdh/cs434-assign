60371f7311130b7b7684f27a749150a1d8200e99
gimdh <36066149+gimdh@users.noreply.github.com>
2020-09-14 23:54:14 +0900

    Fix bug in pascal: c=1, r=0 case may cause unexpected behavior

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -17,7 +17,7 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if (c == 0 || c == r) 1
+    if (c == 0 || r == 0 || c == r) 1
     else pascal(c - 1, r -1) + pascal(c, r - 1)
   }
 
